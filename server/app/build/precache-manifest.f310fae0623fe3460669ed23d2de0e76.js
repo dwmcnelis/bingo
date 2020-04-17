3a592b34e9f23f0560f0a6a2e3dc2009dfcd09ae
Dave McNelis <davemcnelis@gmail.com>
2020-04-17 19:48:46 -0400

    v1

---
+++ server/app/build/precache-manifest.f310fae0623fe3460669ed23d2de0e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba63875ed88e09a6470acdcae17345c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08bc07cb3a722774c97f",
+    "url": "/static/css/main.02bff95b.chunk.css"
+  },
+  {
+    "revision": "6e39892794783ac27c76",
+    "url": "/static/js/2.71f0bdab.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.71f0bdab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08bc07cb3a722774c97f",
+    "url": "/static/js/main.c0da0973.chunk.js"
+  },
+  {
+    "revision": "293a713f54559d4510ee",
+    "url": "/static/js/runtime-main.28936ed5.js"
+  }
+]);
