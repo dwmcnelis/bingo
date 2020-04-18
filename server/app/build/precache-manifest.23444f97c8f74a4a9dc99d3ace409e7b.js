6f43abfb75e510ebfe9569eb2647e8c0ded5232e
Dave McNelis <davemcnelis@market76.com>
2020-04-18 11:45:42 -0400

    update

---
+++ server/app/build/precache-manifest.23444f97c8f74a4a9dc99d3ace409e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194468dadf52e37cecd27d82a0f92295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14125071a24312d48882",
+    "url": "/static/css/main.18c9d590.chunk.css"
+  },
+  {
+    "revision": "6e39892794783ac27c76",
+    "url": "/static/js/2.71f0bdab.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.71f0bdab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14125071a24312d48882",
+    "url": "/static/js/main.d443cf1f.chunk.js"
+  },
+  {
+    "revision": "293a713f54559d4510ee",
+    "url": "/static/js/runtime-main.28936ed5.js"
+  }
+]);
